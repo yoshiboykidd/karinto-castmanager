45fcf1501cb2d5df7aa6607774e3e2bcd18a84c2
yoshiboy <yoshiboykidd@gmail.com>
2026-02-02 12:48:42 +0900

    update v2.8.0

--- push.bat
+++ push.bat
@@ -1,5 +1,9 @@
 @echo off
+
 git add .
-git commit -m "update"
+
+git commit -m "update v2.8.0"
+
 git push
-npx vercel --prod --token JBHlJKE6lvunwyiwyXiYokea
+
+npx vercel --prod --token JBHlJKE6lvunwyiwyXiYokea --org team_LeEOFVqgpSOToSYVJZqqVrIw --project prj_qQnOy6pnoPn8OwUmWqza1fnTFUSb
