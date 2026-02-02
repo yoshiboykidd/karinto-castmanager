6f29fa557deb987b1f5af9e7b613d6d4364542b4
yoshiboy <yoshiboykidd@gmail.com>
2026-02-02 12:50:23 +0900

    fix header and UI v2.8.0

--- push.bat
+++ push.bat
@@ -1,9 +1,33 @@
 @echo off
 
+:: --- ここにメモした情報を貼り付けてください ---
+
+set VERCEL_TOKEN=【JBHlJKE6lvunwyiwyXiYokea】
+
+set VERCEL_ORG_ID=【team_LeEOFVqgpSOToSYVJZqqVrIw】
+
+set VERCEL_PROJECT_ID=【prj_qQnOy6pnoPn8OwUmWqza1fnTFUSb】
+
+:: --------------------------------------------
+
+
+
+:: Node.jsの「日本語PC名エラー」を回避するために、一時的に英語名を名乗る
+
+set COMPUTERNAME=Karinto-PC
+
+
+
+echo 🚀 Git Pushを開始します...
+
 git add .
 
-git commit -m "update v2.8.0"
+git commit -m "fix header and UI v2.8.0"
 
 git push
 
-npx vercel --prod --token JBHlJKE6lvunwyiwyXiYokea --org team_LeEOFVqgpSOToSYVJZqqVrIw --project prj_qQnOy6pnoPn8OwUmWqza1fnTFUSb
+
+
+echo 🚀 Vercelへの本番デプロイを開始します...
+
+npx vercel --prod
