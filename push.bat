bef04623ecf539af06c663391c627edcd79a1339
yoshiboy <yoshiboykidd@gmail.com>
2026-02-04 13:05:32 +0900

    fix: restore specific date UI

--- push.bat
+++ push.bat
@@ -2,12 +2,8 @@
 chcp 65001 > nul
 
 git add .
-git commit -m "fix: restore specific date UI and icons"
+git commit -m "fix: restore specific date UI"
 
-:: 「deploy」という単語を明示し、さらに 「--cwd .」 でカレントディレクトリを強制指定します
-:: トークンは必ず " " で囲ってください
-npx vercel deploy --prod --yes --token "ft2H1u3JFi1rtE856gt1aLki" --cwd .
+npx vercel --prod --yes --token "ft2H1u3JFi1rtE856gt1aLki"
 
-echo ---------------------------------------
-echo 🎉 デプロイが完了しました！
 pause
